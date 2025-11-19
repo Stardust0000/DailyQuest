283493ff0a4e70fa21889c01fc7c52d943071b0e
Stardust0000 <nehanavade1111@gmail.com>
2025-11-19 19:58:09 +0530

    final commit-maybe

--- src/Components/Taskform.jsx
+++ src/Components/Taskform.jsx
@@ -1,24 +1,51 @@
-export default function Taskform() {
+import {useState} from "react"
+
+export default function Taskform({addTask}) {
+    // for task name 
+    const[task, setTasks] = useState('');
+    // for task priority
+    const [priority, setPriority] = useState('medium');
+    // for task category
+    const [category, setCategory] = useState('general');
+
+    const handleSubmit = (e) => {
+        e.preventDefault(); //refresh prevent
+        if(task.trim()) {
+            // If task is not empty and
+            // call the parent’s addTask to update the tasks state
+            addTask({text: task, priority, category, completed: false});
+            // reset the form:
+            setTasks('');
+            setPriority('medium')
+            setCategory('general');
+        }
+    };
+
     return(
-        <form>
-            <div>
-                <input type="text" placeholder="Enter task"/>
+        <form onSubmit={handleSubmit} className="task-form">
+            <div id="inp">
+                <input type="text" 
+                placeholder="Enter task" 
+                value={task} 
+                onChange={(e) => setTasks(e.target.value)}
+                required/>
                 <button type="submit">Add Task</button>
             </div>
             
-            <div>
-                <select>
+            <div id="btns">
+                <select value={priority} 
+                onChange={(e)=>setPriority(e.target.value)}>
                     <option value="high">High</option>
                     <option value="medium">Medium</option>
                     <option value="low">Low</option>
                 </select>
 
-                <select>
+                <select value={category} 
+                onChange={(e) => setCategory(e.target.value)}>
                     <option value="general">General</option>
                     <option value="work">Work</option>
                     <option value="personal">Personal</option>
                 </select>
-
             </div>
         </form>
     )
